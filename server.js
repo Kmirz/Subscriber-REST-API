36024fa4388e95a5785e1b68d60abdfff15403d6
K Mirz <104242241+Kmirz@users.noreply.github.com>
2022-05-06 23:10:04 -0400

    Introducing front end connection to server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
+const cors = require('cors');
+
 const express = require('express')
 const mongoose = require('mongoose')
 const subscribersRouter = require('./routes/subscribers')
@@ -11,6 +13,7 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
+app.use(cors())
 app.use(express.json())
 app.use('/subscribers', subscribersRouter)
 
