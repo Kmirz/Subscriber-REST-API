8e59e7ce501da509d52be06585ec8f3f4e450843
K Mirz <104242241+Kmirz@users.noreply.github.com>
2022-05-06 22:10:23 -0400

    Updating server routes for Subscriber POST

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
-db.once('once', () => console.log('Connected to Database'))
+db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
 
