a95c5ccbe3770487de8272a32dbd47eed6abe40b
K Mirz <104242241+Kmirz@users.noreply.github.com>
2022-05-07 16:36:43 -0400

    Introduced UI to allow for adding and removing of Subscribers from db

---
+++ FrontEnd/subscriberQuery.js
@@ -0,0 +1,80 @@
+const queryAll = async () => {
+  let subscriberList = document.querySelector("#subscriberList");
+  subscriberList.innerHTML = "";
+  const request = await fetch("http://localhost:3000/subscribers")
+    .then((res) => res.json())
+    .then((res) => listSubscribers(res));
+};
+
+const listSubscribers = (res) => {
+  console.log(res);
+
+  res.forEach((element) => {
+    let li = document.createElement("li");
+    li.id = element._id;
+    li.appendChild(
+      document.createTextNode(element.name)
+    );
+    subscriberList.appendChild(li);
+  });
+};
+
+const addSubscriber = async () => {
+  console.log("clicked");
+  let subscriberName = document.querySelector("#SubscriberName").value;
+
+  let userInput = {
+    name: subscriberName,
+    subscribedToChannel: "test",
+  };
+
+  console.log(JSON.stringify(userInput));
+
+  let subscriberInfo = {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userInput),
+  };
+
+  const request = await fetch(
+    "http://localhost:3000/subscribers",
+    subscriberInfo
+  ).catch((err) => console.log(err));
+
+  document.querySelector("#SubscriberName").value = "";
+
+  queryAll();
+};
+
+const deleteSubscriber = async () => {
+  console.log("clicked");
+  let subscriberID = document.querySelector("#SubscriberID").value;
+
+  let fetchURL = "http://localhost:3000/subscribers/" + String(subscriberID);
+
+  let options = {
+    method: "delete",
+  };
+
+  console.log(fetchURL);
+
+  const request = await fetch(fetchURL, options).catch((err) =>
+    console.log(err)
+  );
+
+  document.querySelector("#SubscriberID").value = "";
+
+  queryAll();
+};
+
+queryAll();
+
+document
+  .querySelector("#addSubscriber")
+  .addEventListener("click", addSubscriber);
+
+document
+  .querySelector("#deleteSubscriber")
+  .addEventListener("click", deleteSubscriber);
