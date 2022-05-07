099e870d2f362006f71e173e8cb10113e7d3a732
K Mirz <104242241+Kmirz@users.noreply.github.com>
2022-05-06 22:40:25 -0400

    Refactored grouped imports

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose')
+const subscribersRouter = require('./routes/subscribers')
+
+const app = express()
 
 mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true})
 const db = mongoose.connection
@@ -10,8 +12,6 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
-
-const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
 
 app.listen(3000, () => console.log('Server Started'))
