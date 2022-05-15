0194f8be604c9180377d631ed6dc993eabc0d417
K Mirz <104242241+Kmirz@users.noreply.github.com>
2022-05-15 19:18:19 -0400

    Updating route case

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const express = require("express");
 const mongoose = require("mongoose");
-const subscribersRouter = require("./routes/subscribers");
+const subscribersRouter = require("./Routes/subscribers");
 
 const app = express();
 
